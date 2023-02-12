d409c9d570c3a864e030352672e80906dda267de
Clement Wainaina <Clement.Wainaina@CRAFTSILICON.LOCAL>
2023-02-12 15:58:01 +0300

    First Commit

---
+++ src/main/java/com/stocktrade/demo/StockTradeApiApplication.java
@@ -0,0 +1,13 @@
+package com.stocktrade.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradeApiApplication.class, args);
+	}
+
+}
